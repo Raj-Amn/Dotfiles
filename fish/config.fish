da9e40e6fbf598f31a5454f6abae71a86cfd4edb
Aman Raj <amanraj@Amans-MacBook-Air.local>
2026-02-08 03:19:31 +0530

    fix:cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set -U fish_key_bindings fish_default_key_bindings
-
+# Commands to run in interactive sessions can go here
 end
